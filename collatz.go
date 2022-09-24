2c61dd8faaa94f953be628f38ebcf98ca284f190
Eduardo Penedos <epenedos@icloud.com>
2022-09-24 22:25:41 +0100

    starting

---
+++ collatz.go
@@ -0,0 +1,20 @@
+import (
+	//    "os"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+
+	//    "math/rand"
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+	//    "github.com/go-echarts/go-echarts/v2/types"
+)
+
+func main() {
+
+	
+
+}
