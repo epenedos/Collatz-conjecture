e80007139de5b0d16de75897d8c07f023bd32fbb
Eduardo Penedos <epenedos@icloud.com>
2022-09-25 08:53:32 +0100

    first running version

--- collatz.go
+++ collatz.go
@@ -1,35 +1,37 @@
-package collatz
+package main
 
-import (
-	//    "os"
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
-	"net/url"
-	"strconv"
+import "fmt"
 
-	//    "math/rand"
-	"github.com/go-echarts/go-echarts/v2/charts"
-	"github.com/go-echarts/go-echarts/v2/opts"
-	//    "github.com/go-echarts/go-echarts/v2/types"
-)
+//    "os"
+
+//    "math/rand"
+
+//    "github.com/go-echarts/go-echarts/v2/types"
+
+func coll(r int) (res int) {
+
+	if r%2 == 0 {
+		res = r / 2
+	} else {
+		res = r*3 + 1
+	}
+
+	return res
+}
 
 func main() {
 
-	i := 7
+	var i int = 1000
 
+	fmt.Println(i)
 	for {
-		r = coll(i)
-	}
+		n := coll(i)
+		fmt.Println(n)
 
-}
-
-func coll(r int) int {
-	if r%2 == 0 {
-		r = r % 2
-	} else {
-		r = r*3 + 1
+		if n == 1 {
+			break
+		}
+		i = n
 	}
 
 }
